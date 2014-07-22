7f24603fff1d8e37646c6dabb121a8fee884d621
Fumiya Katoh <katoh.numazu.ct@gmail.com>
2014-07-22 19:44:21 +0900

    App.xamlに固定呼び出しを定義

---
+++ wpf_fixedfps/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace wpf_fixedfps {
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application {
+		private void Application_Startup(object sender, StartupEventArgs e) {
+			var mainWindow = new MainWindow();
+			var isClosedMainWindow = false;
+			mainWindow.Closed += (s, ex) => isClosedMainWindow = true;
+			mainWindow.Show();
+
+			long nextTick = 0;
+			while (!isClosedMainWindow) {
+				var currentTick = Environment.TickCount;
+				/* 更新 */
+				if (currentTick > nextTick) {
+					mainWindow.OnUpdate();
+					//次回実行時刻を指定
+					var span = (long)Math.Floor(1000.0 / mainWindow.FPS);
+					while (currentTick > nextTick) nextTick += span;
+				}
+				DoEvents();
+			}
+		}
+
+		private void DoEvents() {
+			var frame = new DispatcherFrame();
+			DispatcherOperationCallback exitFrameCallback = f => {
+				((DispatcherFrame)f).Continue = false;
+				return null;
+			};
+			// 非同期で実行する
+			var exitOperation = Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, exitFrameCallback, frame);
+
+			// 実行ループを開始する
+			Dispatcher.PushFrame(frame);
+
+			// コールバックが終了していない場合は中断
+			if (exitOperation.Status != DispatcherOperationStatus.Completed) {
+				exitOperation.Abort();
+			}
+		}
+
+		private void Application_Exit(object sender, ExitEventArgs e) {
+
+		}
+	}
+}
---
+++ wpf_fixedfps/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_fixedfps {
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window {
+		/// <summary>
+		/// 固定フレームレート
+		/// </summary>
+		public double FPS = 60;
+		/// <summary>
+		/// FPS固定で必ず呼ばれる
+		/// </summary>
+		public void OnUpdate() {
+			//FPS固定で行いたい処理を記述
+		}
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
+}
